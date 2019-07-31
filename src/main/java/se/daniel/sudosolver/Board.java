333cdd1c2397cd7c381cc1ed87e4ea78b074809f
Daniel Pettersson <daniel.pettersson@accounting.pe>
2019-07-31 23:02:53 +0200

    Added basic logic to query and render board

--- src/main/java/se/daniel/sudosolver/Board.java
+++ src/main/java/se/daniel/sudosolver/Board.java
@@ -1,27 +1,96 @@
 package se.daniel.sudosolver;
 
+import lombok.Getter;
+
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
 class Board {
 
-    private final int[][] board;
+    @Getter
+    private final int[][] rows;
 
-    Board(int[][] board) {
-        this.board = board;
+    Board(int[][] rows) {
+        this.rows = rows;
 
         verifyBoardDimensions();
+        verifyValidBoardNumbers();
+    }
+
+    public List<Integer> getRowNumbers(int row) {
+        return Arrays.stream(rows[row])
+                .filter(v -> v != 0)
+                .boxed()
+                .collect(toList());
+    }
+
+    public List<Integer> getColumnNumbers(int col) {
+        return Arrays.stream(rows)
+                .mapToInt(row -> row[col])
+                .filter(v -> v != 0)
+                .boxed()
+                .collect(toList());
+    }
+
+    public List<Integer> getGridNumbers(int row, int col) {
+        final var gridRow = row / 3;
+        final var gridCol = col / 3;
+
+        return range(gridRow * 3, gridRow * 3 + 3)
+                .flatMap(r -> Arrays.stream(rows[r], gridCol * 3, gridCol * 3 + 3))
+                .filter(v -> v != 0)
+                .boxed()
+                .collect(toList());
     }
 
     private void verifyBoardDimensions() {
 
-        if(board.length != 9) {
+        if(rows.length != 9) {
             throw new IllegalArgumentException("There should be 9 rows in sudoku");
         }
 
-        Arrays.stream(board).forEach(row ->  {
+        Arrays.stream(rows).forEach(row ->  {
             if(row.length != 9) {
                 throw new IllegalArgumentException("There should be 9 columns in sudoku");
             }
         });
+    }
+
+    private void verifyValidBoardNumbers() {
+
+        range(0, 9).forEach(r -> {
+                if (isAnyNumbersDuplicates(getRowNumbers(r))) {
+                    throw new IllegalStateException("Row " + r + " has duplicate numbers");
+                }
+        });
+
+        range(0, 9).forEach(c -> {
+            if (isAnyNumbersDuplicates(getColumnNumbers(c))) {
+                throw new IllegalStateException("Column " + c + " has duplicate numbers");
+            }
+        });
+
+        range(0, 3).forEach(gr -> range(0, 3).forEach(gc -> {
+            if(isAnyNumbersDuplicates(getGridNumbers(gr * 3, gc * 3))) {
+                throw new IllegalStateException("Grid " + gr + "," + gc + " has duplicate numbers");
+            }
+        }));
+
+    }
+
+    private boolean isAnyNumbersDuplicates(List<Integer> list) {
+        final var lump = new HashSet<Integer>();
+
+        for (Integer n : list) {
+            if (!lump.add(n)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/main/java/se/daniel/sudosolver/BoardRenderer.java
+++ src/main/java/se/daniel/sudosolver/BoardRenderer.java
@@ -3,14 +3,21 @@ package se.daniel.sudosolver;
 import lombok.AllArgsConstructor;
 
 import java.io.PrintStream;
+import java.util.Arrays;
 
 @AllArgsConstructor
-public class BoardRenderer {
+class BoardRenderer {
 
     private final PrintStream out;
 
-    public void render(Board board) {
+    void render(Board board) {
 
+        Arrays.stream(board.getRows()).forEach(row -> {
+            Arrays.stream(row).mapToObj(v -> v == 0 ? "*" : Integer.toString(v)).forEach(out::print);
+            out.println("");
+        });
+
+        out.println("---------");
     }
 
 }
